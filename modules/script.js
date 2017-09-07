deb120beaf657436f60eff60689073ca1dc45196
Szymon Miks <miks.szymon@gmail.com>
2017-09-07 23:13:34 +0200

    utworzenie klasy BitMarket i dodanie dwóch funkcji odpowiedzialnych za uruchamiana w cyklicznym czasie zapytania do api

--- modules/script.js
+++ modules/script.js
@@ -1,9 +1,33 @@
-exports.test = function() {
-    // request('https://www.bitmarket.pl/json/LTCPLN/ticker.json', function (error, response, body) {
-    //   console.log('error:', error);
-    //   console.log('statusCode:', response && response.statusCode); 
-    //   const info = JSON.parse(body);
+class BitMarket {
+  constructor() {
+    console.log("bit market");
+  }
+
+  cronRun() {
+    const schedule = require('node-schedule');
+    let test = this.askAPI(function(info){
+      console.log(info);
+    });
+    // var j = schedule.scheduleJob('0 */5 * * * *', () => {
+    //   console.log('New render api function');
     // });
-    console.log("dupa");
-  // return info;
+    console.log("test");
+  }
+
+  askAPI(callback) {
+    const request = require('request');
+    let info;
+    request('https://www.bitmarket.pl/json/LTCPLN/ticker.json', function (error, response, body) {
+      if (error) {
+        console.log('error:', error); // Print the error if one occurred 
+      }
+      console.log('https://www.bitmarket.pl/json/LTCPLN/ticker.json -> statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+      if (body) {
+        info = JSON.parse(body)
+        callback(info);
+      }
+    });
+  }
 }
+
+module.exports = BitMarket;
