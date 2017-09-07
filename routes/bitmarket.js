f42d2c168c250d065fc93bfc49e30e508e2078c7
Szymon Miks <miks.szymon@gmail.com>
2017-09-07 23:15:20 +0200

    próby na razie nie udane ze względu na błędne callback klasy bitmarket, przechwycenia tego co zwraca metoda aspAPI i zapianie do zmiennej w template

--- routes/bitmarket.js
+++ routes/bitmarket.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var BitMarket = require('../modules/script.js');
+
+let bitmarket = new BitMarket()
+let info = bitmarket.cronRun();
+// console.log(info);
+
 router.get('/', function(req, res, next) {
   res.render('bitmarket/page', {
     title: 'Bitmarket',
     text: 'Moje opracowanie api z bitmarket',
-    info: 'info'
+    info: info
   });
 });
 
