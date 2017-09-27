333925eef8688b93837670c7020414e43deda61f
Szymon Miks <miks.szymon@gmail.com>
2017-09-27 22:37:26 +0200

    podwaliny pod zapisywanie wyników do bazy danych

--- routes/snake.js
+++ routes/snake.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/snake', { useMongoClient: true });
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+  nickname: {type: String, required: true},
+  score: {type: Number, required: true}
+},{ collection : 'snake_result' });
+
+var snakeResult = mongoose.model('snake-result', schema);
+
+console.log("snake")
 
 router.get('/', function(req, res, next) {
-  res.render('snake/snake', {
-    title: 'Snake',
-    text: 'Prosta gra Snake'
-  });
+  snakeResult.find()
+    .then(function(result) {
+      console.log(result);
+      res.render('snake/snake', {
+        title: 'Snake',
+        text: 'Prosta gra Snake',
+        result: result
+      });
+    });
 });
 
 module.exports = router;
