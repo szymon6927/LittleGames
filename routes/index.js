4ace704af8578d387e7366e0e56743daa51db2aa
Szymon Miks <miks.szymon@gmail.com>
2017-11-22 23:55:20 +0100

    dodanie obiektu z grami, ich opisami url itp

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+GAMES = {
+  snake: {
+    title: 'Snake',
+    description: 'Zagraj w najpopularnijeszą grę wszechczasów, znanego i słynnego węża. Zdobądź jak najwięcej puntków i zostań mistrzem',
+    image: '/img/snakes-and-ladders.svg',
+    url: '/snake'
+  },
+  tictactoe: {
+    title: 'Kółko i krzyżyk',
+    description: 'Zagraj w kółko i krzyżyk. Sprawdź czy uda Ci się pokonać najlepszego gracza jakim jest komputer',
+    image: '/img/tictactoe.svg',
+    url: '/tictactoe'
+  }
+}
+
 router.get('/', function(req, res, next) {
   res.render('index', { 
     welcomeMessage: "Witaj na stronie projektu LittleGames",
-    chooseInfo: "Wybierz grę w którą chcesz zagrać"
+    chooseInfo: "Wybierz grę w którą chcesz zagrać",
+    games: GAMES
    });
 });
 
