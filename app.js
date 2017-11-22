2b8d008d15aef3adf0a40025384456c5bc6d2742
Szymon Miks <miks.szymon@gmail.com>
2017-11-22 23:54:24 +0100

    wlasny if z róznymi operatorami

--- app.js
+++ app.js
@@ -24,7 +24,33 @@ var hbs = expressHbs.create({
   defaultLayout: 'layout', 
   extname: '.hbs',
   helpers: {
-    inc: function (value) {  return parseInt(value) + 1; }
+    inc: function (value) {  return parseInt(value) + 1; },
+    ifCond: function (v1, operator, v2, options) {
+      switch (operator) {
+        case '==':
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+          return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+          return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+          return options.inverse(this);
+      }
+    }
   }
 });
 // view engine setup
